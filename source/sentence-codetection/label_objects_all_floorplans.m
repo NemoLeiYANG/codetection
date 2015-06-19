7a42d9418a7f1f95360be6d6348d330a5ff614fd
Scott Bronikowski <scottbronikowski@gmail.com>
2015-06-19 16:40:13 -0400

    Minor cleanup of label_objects_all_floorplans.m

--- source/sentence-codetection/label_objects_all_floorplans.m
+++ source/sentence-codetection/label_objects_all_floorplans.m
@@ -100,8 +100,62 @@ for i = 1:M
     end %for j
 end %for i
 
-end %function
+%now look at avg_similarity values to determine which are alike
+%diagonal elements are self-similarity--look in row and column to see if
+%any other values are higher
 
+%%START HERE%%
+ 
+% unique_label = 1; %first unique label value
+% labels = zeros(M,1);
+% while (min(labels) == 0) %keep going until all labels set
+%     for i = 1:M
+%         if (labels(i) == 0)  %only do stuff if label not already set
+%             [~,rowidx] = max(avg_similarity_matrix(i,:));
+%             [~,colidx] = max(avg_similarity_matrix(:,i));
+%             if ((rowidx == i) && (colidx == i)) %we have a new unique label
+%                 labels(i) = unique_label;
+%                 unique_label = unique_label + 1;
+%             elseif (colidx ~= i) %copy label from colidx
+%                 labels(i) = labels(colidx);
+%             elseif (rowidx ~= i) %copy label from rowidx
+%                 labels(i) = labels(rowidx);
+%             else
+%                 fprintf('THIS SHOULDN''T HAPPEN');
+%             end % if row && col
+%         end %if        
+%     end %for i
+% end %while
+% xy_with_label(:,3) = labels; %done with this
+% 
+% %now do sorting and saving
+% outfilename = strcat(img_dir,'/object_xy_with_label.mat');
+% save(outfilename,'xy_with_label'); %object locations/labels saved
+% outfilename2 = strcat(img_dir,'/phow_hist_fvcell.mat');
+% save(outfilename2,'fvcell'); %phow histograms saved to file
+% 
+% for i = 1:(unique_label - 1)
+%     new_dir = strcat(img_dir,'fplabel',num2str(i),'/');
+%     if (exist(new_dir,'dir'))
+%         rmdir(new_dir,'s'); %get rid of old data
+%     end %if
+%     mkdir(new_dir); %make fplabel directories
+% end %for i
+% 
+% for i = 1:M %for each temp label
+%     src_dir = strcat(img_dir,'tmp',num2str(i),'/');
+%     src_file_list = dir(src_dir);
+%     num_files = numel(src_file_list) - 2; % -2 is because of . and ..
+%     dest_dir = strcat(img_dir,'fplabel',num2str(xy_with_label(i,3)),'/');
+%     for j = 1:num_files %for each image in tmpN dir
+%         src = strcat(src_dir,src_file_list(j+2).name);
+%         dest = strcat(dest_dir,src_file_list(j+2).name);
+%         movefile(src,dest);
+%     end %for j
+%     rmdir(src_dir,'s');
+% end %for i
+
+end %function (main)
 
 function [i_out,j_out] = find_indices(i_in,M)
     len = numel(M);
@@ -115,9 +169,8 @@ function [i_out,j_out] = find_indices(i_in,M)
     end %for a
 end %function
 
-
-function old_shit
-%%%%%%%%%%%%%%%%%%%%%%%OLD STUFF
+%%%%%%%%%%%%%%%%%%%%%%%OLD HEADER COMMENTS FROM
+%%%%%%%%%%%%%%%%%%%%%%%SORT_CLUSTERS_SINGLE_FLOORPLAN
 %This function takes the images sorted into clusters (tmpN directories) and
 %finds the like objects, then re-sorts the images into new directories and
 %returns the xy locations with labels (like objects labeled alike)
@@ -130,107 +183,3 @@ function old_shit
 %output: xy_with_label M x 3 matrix of [x y label]
 
 %%???could call sort_by_cluster here if I add cluster_data as an argument
-
-[M,~] = size(objectxys);
-xy_with_label = zeros(M,3);
-xy_with_label(:,1:2) = objectxys;
-ssize = 64; %HARDCODED ssize: the size to which each proposal is rescaled 
-%               to (for phow_hist); ssize should be 2^i; increase i to 
-%               trade efficiency for accuracy
-
-%create cell array for storing feature vectors from each image
-fvcell = cell(M,1);
-
-%add paths for phow stuff
-addpath(genpath('vlfeat/toolbox'));
-run('/home/sbroniko/codetection/source/sentence-codetection/vlfeat/toolbox/vl_setup');
-
-%loop through 1:M, adding feature vectors for each image to a matrix
-for i = 1:M %for each temp label
-    tmp_img_dir = strcat(img_dir,'tmp',num2str(i),'/');
-    tmp_file_list = dir(tmp_img_dir);
-    num_files = numel(tmp_file_list) - 2; % -2 is because of . and ..
-    tmp_phists = zeros(num_files, 12000,'single'); %HARDCODED for phist size
-    for j = 1:num_files %for each image in tmpN dir
-        img = imread(strcat(tmp_img_dir,tmp_file_list(j+2).name));
-        hist_out = phow_hist(img,ssize);
-        tmp_phists(j,:) = hist_out';
-    end %for j
-    fvcell{i} = tmp_phists;
-end %for i
-
-%now do comparison of feature vectors between each pair of temp labels
-avg_similarity_matrix = zeros(M,'single');
-for i = 1:M
-    [num_img_i,~] = size(fvcell{i});
-    for j = i:M %trying with self-similarity instead of i+1:M
-        [num_img_j,~] = size(fvcell{j});
-        simi_matrix = zeros(num_img_i,num_img_j,'single');
-        for k = 1:num_img_i
-            hist1 = fvcell{i}(k,:);
-            for l = 1:num_img_j
-                hist2 = fvcell{j}(l,:);
-                simi_matrix(k,l)= 1 - pdist2(hist1,hist2,'chisq');
-            end %for l
-        end %for k
-        avg_simi = max(mean(simi_matrix,1));
-        avg_simi2 = max(mean(simi_matrix,2));
-        avg_similarity_matrix(i,j) = avg_simi;
-        avg_similarity_matrix(j,i) = avg_simi2; 
-    end %for j
-end %for i
-
-%now look at avg_similarity values to determine which are alike
-%diagonal elements are self-similarity--look in row and column to see if
-%any other values are higher
-
-unique_label = 1; %first unique label value
-labels = zeros(M,1);
-while (min(labels) == 0) %keep going until all labels set
-    for i = 1:M
-        if (labels(i) == 0)  %only do stuff if label not already set
-            [~,rowidx] = max(avg_similarity_matrix(i,:));
-            [~,colidx] = max(avg_similarity_matrix(:,i));
-            if ((rowidx == i) && (colidx == i)) %we have a new unique label
-                labels(i) = unique_label;
-                unique_label = unique_label + 1;
-            elseif (colidx ~= i) %copy label from colidx
-                labels(i) = labels(colidx);
-            elseif (rowidx ~= i) %copy label from rowidx
-                labels(i) = labels(rowidx);
-            else
-                fprintf('THIS SHOULDN''T HAPPEN');
-            end % if row && col
-        end %if        
-    end %for i
-end %while
-xy_with_label(:,3) = labels; %done with this
-
-%now do sorting and saving
-outfilename = strcat(img_dir,'/object_xy_with_label.mat');
-save(outfilename,'xy_with_label'); %object locations/labels saved
-outfilename2 = strcat(img_dir,'/phow_hist_fvcell.mat');
-save(outfilename2,'fvcell'); %phow histograms saved to file
-
-for i = 1:(unique_label - 1)
-    new_dir = strcat(img_dir,'fplabel',num2str(i),'/');
-    if (exist(new_dir,'dir'))
-        rmdir(new_dir,'s'); %get rid of old data
-    end %if
-    mkdir(new_dir); %make fplabel directories
-end %for i
-
-for i = 1:M %for each temp label
-    src_dir = strcat(img_dir,'tmp',num2str(i),'/');
-    src_file_list = dir(src_dir);
-    num_files = numel(src_file_list) - 2; % -2 is because of . and ..
-    dest_dir = strcat(img_dir,'fplabel',num2str(xy_with_label(i,3)),'/');
-    for j = 1:num_files %for each image in tmpN dir
-        src = strcat(src_dir,src_file_list(j+2).name);
-        dest = strcat(dest_dir,src_file_list(j+2).name);
-        movefile(src,dest);
-    end %for j
-    rmdir(src_dir,'s');
-end %for i
-
-end %function
