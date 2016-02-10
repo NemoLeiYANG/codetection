5070da0b6901c3f2745b1a94ff5b372ef7e2da77
Scott Bronikowski <scottbronikowski@gmail.com>
2016-02-10 17:26:47 -0500

    10 Feb 15
    --Thinking about objectness measure-->why not try giving JUST the box contents
      (maybe resized) to the objectness code, letting it run all methods (including
      MS) and then taking the score of the LARGEST AREA box, since that is the one
      that would most closely match the original box.  Or take the mean score of the
      top N boxes.
    
    --Talked with Dan about idea above.  Agreed that mean score would not be very
      principled.  But could take the product of the box score and its IoU with the
      original box.  Also need to look at best-score box--test box with just wall is
      getting a perfect score--try throwing away a box if its area is > 90% (??) of
      full image area.
    
    --Incorporated the above into a rewrite of find_img_objectness_score.m.
    
    --Discovered that the output of the runObjectness is non-deterministic--probably
      because the boxes that are generated are so.  Going to try increasing the
      number of boxes generated to see if it gives a more stable output for my img
      objectness function. **REALIZED that this won't help if I'm looking at the max
      score-->adding more boxes will just increase the likelihood of a wild max
      being in the mix.
    
      **looking at whether the bad maxes for the all-wall box happen at a certain
        IoU value
      **also thinking about some type of Monte Carlo modification--maybe get the max
        3-5 times and then take the median of those
      **or maybe take the median of the original data, instead of the max
    
    **OR DO I EVEN NEED TO WORRY ABOUT BOGUS SCORES-->since I'm going to be using
      the score for a TUBE (i.e., a series of frames), would the averaging at the
      tube level take care of bad results?
    
    --After running SEVERAL tests by hand, observed that the max score for the
      all-wall box was consistently ~2e-2, while the score for goodbox_corners(5,:)
      was ~4e-1.  That seems like a big enough delta to make a difference.
    
    --Tried to see if there was a way to tune the olthresh value to block out the
      occasional bad score on the all-wall box, but after looking at the overlaps of
      the bad all-wall boxes and the overlaps of the max winning boxes on
      goodbox_corners, realized there wasn't a gap in values there-->bad boxes had
      an overlap of ~.75, while good ones had overlap of ~.8-->can't filter on
      that.  May just have to deal with the occasional bad data and let the
      averaging across a tube take care of it
    **Could we take the MEDIAN score of a tube instead of the mean??
    
    --Currently using the myparams structure as the parameters for my runs-->cues
      are MS CC SS, with *no* ED.  Seems to work the best.  Saved in
      objectness_params.mat.  NEED TO REMEMBER TO USE THIS ONE AND NOT params.

--- source/new-sentence-codetection/compute_tube_objectness_score.m
+++ source/new-sentence-codetection/compute_tube_objectness_score.m
@@ -16,13 +16,3 @@ parfor n = 1:nframes
 end %parfor
 end %function
         
-function [x1,y1,x2,y2] = resize_box(box, im_height, im_width, scale)
-cx = (box(1)+box(3)) / 2;
-cy = (box(2)+box(4)) / 2;
-new_w = (box(3)-box(1)+1) * scale;
-new_h = (box(4)-box(2)+1) * scale;
-x1 = int32(max(1, cx-new_w/2));
-y1 = int32(max(1, cy-new_h/2));
-x2 = int32(min(im_width, cx+new_w/2));
-y2 = int32(min(im_height, cy+new_h/2));
-end %function
--- source/new-sentence-codetection/find_img_objectness_score.m
+++ source/new-sentence-codetection/find_img_objectness_score.m
@@ -1,8 +1,67 @@
-function img_score = find_img_objectness_score(img,box,cues,params)
-scores = zeros(1,length(cues));
-for i = 1:length(cues)
-    foo = computeScores(img,cues{i},params,box);
-    scores(i) = foo(5);
-end %for i
-img_score = integrateBayes(cues,scores,params);
+function img_score = find_img_objectness_score(img,box,cues,params) %(img,box,cues,params)
+% scores = zeros(1,length(cues));
+% for i = 1:length(cues)
+%     foo = computeScores(img,cues{i},params,box);
+%     scores(i) = foo(5);
+% end %for i
+% img_score = integrateBayes(cues,scores,params);
+[h,w,~] = size(img);
+scale = 1.2; %HARDCODING image scale factor
+numboxes = 10; %HARDCODING number of boxes to output
+%convert original image box into x y w h
+origbox = [1,1,(box(3)-box(1)+1),(box(4)-box(2)+1)];
+origboxarea = origbox(3)*origbox(4);
+%give box some margins
+[x1,y1,x2,y2] = resize_box(box,h,w,scale);
+boxscores = runObjectness(img(y1:y2,x1:x2,:),numboxes,params);
+%boxes come out as [xmin ymin xmax ymax score], so convert to [x y w h s]
+% for use with rectint
+boxscores(:,3) = boxscores(:,3) - boxscores(:,1) + 1;
+boxscores(:,4) = boxscores(:,4) - boxscores(:,2) + 1;
+%boxscores
+%convert new img box to x y w h
+imgbox = double([1,1,(x2-x1+1),(y2-y1+1)]);
+imgboxarea = imgbox(3)*imgbox(4);
+boxscores(:,6) = boxscores(:,3) .* boxscores(:,4); %box areas
+boxscores(:,7) = rectint(boxscores(:,1:4),origbox); %intersections
+boxscores(:,8)=boxscores(:,6)./imgboxarea; %overlap w/expanded box
+%boxscores(:,8)
+%get rid of boxes that overlap by > thresh
+%key to ratio of origbox to imgbox
+olthresh = 0.9;
+idx = boxscores(:,8) > olthresh;
+boxscores(idx,:) = [];
+%now do IoU for remaining boxes
+boxscores(:,9) = boxscores(:,7) ./ ... %IoU for ORIGINAL box
+    (boxscores(:,6) + origboxarea - boxscores(:,7));
+boxscores(:,8)
+boxscores(:,5)
+boxscores(:,9)
+%multiply IoU by original objectness score
+newscores = boxscores(:,9) .* boxscores(:,5)
+
+imgboxarea
+origboxarea
+1.1*origboxarea/imgboxarea
+
+img_score = [mean(newscores), median(newscores), max(newscores)];
+
+% meanscore = mean(boxscores(:,5));
+% areas = (boxes(:,3)-boxes(:,1)) .* (boxes(:,4)-boxes(:,2));
+% areascores = [areas scores];
+% areascores = sortrows(areascores);
+% boxscores(1,:)
+% img_score = [meanscore,areascores(numboxes,2)];
+
+end %function
+
+function [x1,y1,x2,y2] = resize_box(box, im_height, im_width, scale)
+cx = (box(1)+box(3)) / 2;
+cy = (box(2)+box(4)) / 2;
+new_w = (box(3)-box(1)+1) * scale;
+new_h = (box(4)-box(2)+1) * scale;
+x1 = int32(max(1, cx-new_w/2));
+y1 = int32(max(1, cy-new_h/2));
+x2 = int32(min(im_width, cx+new_w/2));
+y2 = int32(min(im_height, cy+new_h/2));
 end %function
